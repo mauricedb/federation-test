fe0bb4b1b1b251a735319b196357b37c9e32bb71
Maurice de Beijer <maurice.de.beijer@gmail.com>
2021-10-23 15:06:59 +0200

    Main with ModuleFederationPlugin

--- main/next.config.js
+++ main/next.config.js
@@ -1,3 +1,33 @@
-module.exports = {
-  reactStrictMode: true,
+// module.exports = {
+//   reactStrictMode: true,
+
+  
+// }
+
+
+
+module.exports = (phase, { defaultConfig }) => {
+  /**
+   * @type {import('next').NextConfig}
+   */
+  const nextConfig = {
+    reactStrictMode: true,
+    webpack(config, options) {
+      const { ModuleFederationPlugin } = options.webpack.container;
+
+      config.plugins.push(
+        new ModuleFederationPlugin({
+          remotes: {
+            // app2: "app2@https://kevinmfe.s3.amazonaws.com/app2/dist/remoteEntry.js",
+          },
+          shared: [],
+        })
+      );
+
+      return config;
+    }
+  }
+
+  return nextConfig
 }
+
